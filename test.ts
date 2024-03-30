37019ef80d5e03475f1e5084cc37ca724d21ed97
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-30 02:46:48 +0200

    *

--- test.ts
+++ test.ts
@@ -1,82 +1,97 @@
 
-// Dependent types
-type ArrayLength<T, Length extends number> = [T, ...Array<T>] & { length: Length };
+// // Dependent types
+// type ArrayLength<T, Length extends number> = [T, ...Array<T>] & { length: Length };
 
-function sumaArray(array: ArrayLength<number, 3>): number {
-    return array.reduce((acc, val) => acc + val, 0);
-}
+// function sumaArray(array: ArrayLength<number, 3>): number {
+//     return array.reduce((acc, val) => acc + val, 0);
+// }
 
-const array1: ArrayLength<number, 3> = [1, 2, 3]; 
-const array2: ArrayLength<number, 4> = [1, 2, 3, 4]; 
-const array3: ArrayLength<number, 2> = [1, 2]; 
+// const array1: ArrayLength<number, 3> = [1, 2, 3]; 
+// const array2: ArrayLength<number, 4> = [1, 2, 3, 4]; 
+// const array3: ArrayLength<number, 2> = [1, 2]; 
 
-for (let i = 0; i <= 3; i++) {
-  console.log(array1[i]); // error because array1 has length 3, 1,2,3, undefined
+// for (let i = 0; i <= 3; i++) {
+//   console.log(array1[i]); // error because array1 has length 3, 1,2,3, undefined
 
-}
+// }
 
-console.log(sumaArray(array1)); 
-// console.log(sumaArray(array2));  // error
-
-
-
-// Recursive types
+// console.log(sumaArray(array1)); 
+// // console.log(sumaArray(array2));  // error
 
 
-type List<T> = {
-  value: T;
-  next: List<T> | null;
-}
+
+// // Recursive types
 
 
-const createLinkedList = <T>(values: T[]): List<T> | null  => {
-  let list: List<T> | null = null;
-  for (let i = values.length - 1; i >= 0; i--) {
-    list = { value: values[i], next: list };
+// type List<T> = {
+//   value: T;
+//   next: List<T> | null;
+// }
+
+
+// const createLinkedList = <T>(values: T[]): List<T> | null  => {
+//   let list: List<T> | null = null;
+//   for (let i = values.length - 1; i >= 0; i--) {
+//     list = { value: values[i], next: list };
+//   }
+//   return list
+// }
+
+// const list = createLinkedList([1, 2, 3, 4, 5]);
+// console.log(list);
+
+// const linkedList: List<number> = {
+//   value: 1,
+//   next: {
+//     value: 2,
+//     next: {
+//       value: 3,
+//       next: null
+//     }
+//   }
+// }
+
+
+// // Polymorphic types (and ad-hoc polymorphism)
+
+// type Filter = {
+//   <T>(array: T[], f: (item: T) => boolean): T[]
+// }
+
+// const filter: Filter = (array, f) => {
+//   const result: Parameters<typeof f>[0][] = [];
+//  for (let i = 0; i < array.length; i++) {
+//     const item = array[i];
+//     if (f(item)) {
+//       result.push(item);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(filter([1, 2, 3, 4, 5], _ => _ > 2)); // [3, 4, 5]
+
+
+// function len(str: string): number;
+// function len(arr: any[]): number;
+// function len(x: any): number {
+//   return x.length;
+// }
+
+// console.log(len('hello')); // 5
+// console.log(len([1, 2, 3, 4, 5])); // 5
+
+
+function max(m: number, n: number): number {
+  if (m === 0) {
+      return n;
+  } else {
+      return max(m - 1, n > 0 ? n - 1 : n);
   }
-  return list
 }
 
-const list = createLinkedList([1, 2, 3, 4, 5]);
-console.log(list);
-
-const linkedList: List<number> = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: null
-    }
-  }
-}
-
-
-// Polymorphic types (and ad-hoc polymorphism)
-
-type Filter = {
-  <T>(array: T[], f: (item: T) => boolean): T[]
-}
-
-const filter: Filter = (array, f) => {
-  const result: Parameters<typeof f>[0][] = [];
- for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-    if (f(item)) {
-      result.push(item);
-    }
-  }
-  return result;
-}
-
-console.log(filter([1, 2, 3, 4, 5], _ => _ > 2)); // [3, 4, 5]
-
-
-function len(str: string): number;
-function len(arr: any[]): number;
-function len(x: any): number {
-  return x.length;
-}
-
-console.log(len('hello')); // 5
-console.log(len([1, 2, 3, 4, 5])); // 5
+// Exemplo de uso
+console.log(max(5, 10)); // Saída: 10
+console.log(max(0, 7)); // Saída: 7
+console.log(max(3, 3)); // Saída: 3
+console.log(max(10, 5)); // Saída: 10
